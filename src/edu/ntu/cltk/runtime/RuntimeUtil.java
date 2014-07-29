0a0358c6b26e19b24d3550415d1cbc41abadd799
pillar <pillar@pillar-Precision-T3600>
2014-07-29 23:02:15 +0800

    fdsa

--- src/edu/ntu/cltk/runtime/RuntimeUtil.java
+++ src/edu/ntu/cltk/runtime/RuntimeUtil.java
@@ -16,6 +16,57 @@ public class RuntimeUtil {
 		executeProcess(false, cmdArray);
 	}
 	
+	/**
+	 * Execute a process with message returned
+	 * @param args
+	 * @return
+	 */
+	public static String executeProcessWithResponse(String...args){
+		BufferedReader reader = null;
+		Process pro = null;
+		StringBuilder response = new StringBuilder();
+		try {
+			pro = Runtime.getRuntime().exec(args);
+			pro.waitFor();
+			reader = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+			String line = "";
+			while ((line = reader.readLine())!= null) {
+				response.append(line+"\n");
+			}
+			return response.toString();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			if (reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (pro != null){
+				try {
+					pro.getInputStream().close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				pro.destroy();
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Execute a process with printing out the message determined by output
+	 * @param output
+	 * @param args
+	 */
 	public static void executeProcess(boolean output, String... args){
 		BufferedReader reader = null;
 		Process pro = null;
