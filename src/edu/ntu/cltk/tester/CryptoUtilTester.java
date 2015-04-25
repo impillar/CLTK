cad9e581eb3bad4e3fcdb77d35bf6c6cbd2dff95
impillar <droidecho2013@gmail.com>
2015-04-25 17:43:30 +0800

    test

---
+++ src/edu/ntu/cltk/tester/CryptoUtilTester.java
@@ -0,0 +1,28 @@
+package edu.ntu.cltk.tester;
+
+import java.util.List;
+
+import edu.ntu.cltk.crypto.CryptoUtil;
+import edu.ntu.cltk.file.FileUtil;
+
+public class CryptoUtilTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		hexToCharTester();
+	}
+	
+	public static void hexToCharTester(){
+		//String str = "\\x41jfdabc";
+		//System.out.println(CryptoUtil.hexToChar(str));
+		//System.out.println(CryptoUtil.hexToChar(str));
+		List<String> lines = FileUtil.readFileLineByLine("javascript.php");
+		for (String line : lines){
+			FileUtil.writeFile("converted.php", CryptoUtil.hexToChar(line), "a");
+		}
+	}
+
+}
